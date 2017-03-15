b910fd26478afac1fbff3a6c061d40925f445da3
kiranpaturi <kiran_p@k3informatics.com>
2017-03-15 11:51:47 +0530

    added code

---
+++ app.js
@@ -0,0 +1,15 @@
+//variable declarations.
+
+var username = "kiran";
+var age = 30;
+var iAgree = false;
+var calculate = function () {
+
+};
+var users = [];
+var person = {}; //object literal.
+
+function getUsers(abc, zyx) {
+    person.firstname = "kiran";
+    person.lastname = "paturi";
+}
